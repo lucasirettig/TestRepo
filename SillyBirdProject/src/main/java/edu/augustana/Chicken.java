d125fbed70d7e2f1f0019a0e295375fb47caeb32
Lucas Rettig <lucasrettig23@augustana.edu>
2024-09-24 12:04:40 -0500

    Now with dancing chicken

---
+++ SillyBirdProject/src/main/java/edu/augustana/Chicken.java
@@ -0,0 +1,11 @@
+package edu.augustana;
+
+public class Chicken {
+
+    public Chicken(){
+    }
+
+    public void dance(){
+        System.out.println("Chicken Dance!");
+    }
+}
--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -1,7 +1,10 @@
 package edu.augustana;
 
 public class Main {
+
     public static void main(String[] args) {
+        Chicken chick = new Chicken();
         System.out.println("Hello bird world!");
+        chick.dance();
     }
 }
